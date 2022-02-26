2857b926e8fc8afee13666c9142e2cdbcd98e93a
Bassem Dimassi <dimassibassem99@gmail.com>
2022-02-27 00:24:32 +0100

    Create NarrowSidebar

---
+++ pages/_document.js
@@ -0,0 +1,12 @@
+import {Html, Head, Main, NextScript} from 'next/document'
+
+export default function Document() {
+    return (<Html className="h-full bg-gray-50" lang="en">
+        {/* eslint-disable-next-line @next/next/no-title-in-document-head */}
+        <Head><title>My page title</title></Head>
+        <body className="h-full overflow-hidden">
+        <Main/>
+        <NextScript/>
+        </body>
+    </Html>)
+}
