8b97948f59fa32fa06fa9154d8843fa0eae060da
Bassem Dimassi <dimassibassem99@gmail.com>
2022-02-27 01:36:35 +0100

    Create ProfileDropdown

--- components/ContentArea.js
+++ components/ContentArea.js
@@ -10,6 +10,7 @@ import {
 import {Menu, Transition} from "@headlessui/react";
 import {classNames} from "../utils";
 import MainContent from "./MainContent";
+import ProfileDropdown from "./ProfileDropdown";
 
 const ContentArea = ({setMobileMenuOpen, userNavigation, currentFile, files, tabs}) => {
     return (
@@ -57,48 +58,9 @@ const ContentArea = ({setMobileMenuOpen, userNavigation, currentFile, files, tab
                             </form>
                         </div>
                         <div className="ml-2 flex items-center space-x-4 sm:ml-6 sm:space-x-6">
+
                             {/* Profile dropdown */}
-                            <Menu as="div" className="relative flex-shrink-0">
-                                <div>
-                                    <Menu.Button
-                                        className="bg-white rounded-full flex text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                                        <span className="sr-only">Open user menu</span>
-                                        <img
-                                            className="h-8 w-8 rounded-full"
-                                            src="https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80"
-                                            alt=""
-                                        />
-                                    </Menu.Button>
-                                </div>
-                                <Transition
-                                    as={Fragment}
-                                    enter="transition ease-out duration-100"
-                                    enterFrom="transform opacity-0 scale-95"
-                                    enterTo="transform opacity-100 scale-100"
-                                    leave="transition ease-in duration-75"
-                                    leaveFrom="transform opacity-100 scale-100"
-                                    leaveTo="transform opacity-0 scale-95"
-                                >
-                                    <Menu.Items
-                                        className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                                        {userNavigation.map((item) => (
-                                            <Menu.Item key={item.name}>
-                                                {({active}) => (
-                                                    <a
-                                                        href={item.href}
-                                                        className={classNames(
-                                                            active ? 'bg-gray-100' : '',
-                                                            'block px-4 py-2 text-sm text-gray-700'
-                                                        )}
-                                                    >
-                                                        {item.name}
-                                                    </a>
-                                                )}
-                                            </Menu.Item>
-                                        ))}
-                                    </Menu.Items>
-                                </Transition>
-                            </Menu>
+                            <ProfileDropdown userNavigation={userNavigation}/>
 
                             <button
                                 type="button"
