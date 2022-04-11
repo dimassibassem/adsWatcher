cabff60fcf548d6a618bfd872b04dccd5fb186a9
Bassem Dimassi <dimassibassem99@gmail.com>
2022-04-11 13:46:21 +0100

    migrations

---
+++ server/prisma/migrations/20220410090205_adding_favorite_table/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "Favorite" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "articleId" INTEGER NOT NULL,
+
+    CONSTRAINT "Favorite_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Favorite" ADD CONSTRAINT "Favorite_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Favorite" ADD CONSTRAINT "Favorite_articleId_fkey" FOREIGN KEY ("articleId") REFERENCES "Article"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ server/prisma/migrations/20220410092306_maintain_favorite/migration.sql
@@ -0,0 +1,5 @@
+-- DropForeignKey
+ALTER TABLE "Favorite" DROP CONSTRAINT "Favorite_articleId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Favorite" DROP CONSTRAINT "Favorite_userId_fkey";
