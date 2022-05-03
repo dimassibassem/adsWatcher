866fd1edbf27ced94e92ffb1a7c7d1a5a760604d
Bassem Dimassi <dimassibassem99@gmail.com>
2022-05-03 05:11:59 +0100

    replacing a tag with Link/next

--- client/components/ProfileDropdown.js
+++ client/components/ProfileDropdown.js
@@ -1,15 +1,13 @@
 import React, {Fragment} from 'react';
 import {Menu, Transition} from "@headlessui/react";
-import {classNames} from "../utils";
 import {useLocalStorage, useStore} from "../store";
+import Link from "next/link";
 
 
 const ProfileDropdown = () => {
     const userNavigation = useStore(store => store.userNavigation)
     const setToken = useLocalStorage(store => store.setToken);
-
     const userData = useStore(store => store.userData);
-
     let profileAvatar = userData?.avatarUrl ? userData.avatarUrl : "https://www.linkpicture.com/q/avatar_3.png";
 
     return (
@@ -41,25 +39,20 @@ const ProfileDropdown = () => {
                 >
                     <Menu.Items
                         className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                        {/*todo: change <a> to Link tag handling css*/}
                         {userNavigation.map((item) => (
                             <Menu.Item key={item.name}>
-                                {({active}) => (
-                                    <a
-                                        href={item.href}
-                                        onClick={() => {
-                                            if (item.name === "Sign out") {
-                                                setToken(null);
-                                            }
-                                        }}
-                                        className={classNames(
-                                            active ? 'bg-gray-100' : '',
-                                            'block px-4 py-2 text-sm text-gray-700'
-                                        )}
+                                <Link href={item.href} passHref>
+                                    <a className=" hover:text-xl hover:bg-gray-100 block px-4 py-2 text-sm text-gray-700"
+                                       onClick={() => {
+                                           if (item.name === "Sign out") {
+                                               setToken(null);
+                                           }
+                                       }}
                                     >
                                         {item.name}
                                     </a>
-                                )}
+                                </Link>
+
                             </Menu.Item>
                         ))}
                     </Menu.Items>
